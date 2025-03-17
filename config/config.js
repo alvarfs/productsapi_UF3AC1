04914a9bc4e01db6e2fd4c0f1c7dcf5a3781d7a0
alvarfs <146431698+alvarfs@users.noreply.github.com>
2025-03-17 21:42:35 +0100

    test URI

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://user:1234@cluster0.k09vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  // mongodb+srv://user:1234@cluster0.k09vf.mongodb.net/
-  // s8c8ikv2uozle2o8lut67h5k6fb5xm
+  // mongoURI: "mongodb+srv://user:1234@cluster0.k09vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  mongoURI: process.env.MONGOURI
 };
