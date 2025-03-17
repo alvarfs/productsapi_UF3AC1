f7d7e2272016338f3f7d6997915521c353e09ae4
alvarfs <146431698+alvarfs@users.noreply.github.com>
2025-03-17 21:19:09 +0100

    test slugify

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const products_routes = require('./routes/products.js')
+const slugify = require('slugify');
 
 //Server instantiation
 const app = express()
@@ -14,8 +15,13 @@ app.use(express.json())
 app.use('/', products_routes)
 
 //Server startup
+// app.listen(5000, () => {
+//     console.log('server is listening on port 5000')
+// })
+
+const message = "server is listening on port 5000";
+const slugMessage = slugify(message, { replacement: "*", lower: true });
+
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
-
-
+    console.log(slugMessage);
+});
