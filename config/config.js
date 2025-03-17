fafae7f3cf0e9b63cb6712dbc0ee1cdcf91c6c15
alvarfs <146431698+alvarfs@users.noreply.github.com>
2025-03-17 20:40:02 +0100

    URI

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: ""
+  mongoURI: "mongodb+srv://user:1234@cluster0.k09vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  // mongodb+srv://user:1234@cluster0.k09vf.mongodb.net/
+  // s8c8ikv2uozle2o8lut67h5k6fb5xm
 };
